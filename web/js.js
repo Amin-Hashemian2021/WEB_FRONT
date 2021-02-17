d80f008f01f299d877bc3aa80e87572554feb256
Amin-Hashemian2021 <78362239+Amin-Hashemian2021@users.noreply.github.com>
2021-02-17 22:47:39 +0300

    Add files via upload

---
+++ web/js.js
@@ -0,0 +1,21 @@
+function a(){
+document.getElementById("data").src="photo.png";
+
+
+
+
+}
+function b(){
+document.getElementById("data").src="eli.png";
+
+
+
+
+}
+function c(){
+document.getElementById("demo").style.backgroundColor="red";
+
+
+
+
+}
